7a944a34d7743f2bc0d86f0e26236057865b3c4f
cyzzc <80954721+cyzzc@users.noreply.github.com>
2023-06-22 17:07:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,11 +24,6 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 # sed -i 's/5a2559f0648198c290bb8839b9f6a0adab8ebcdc/d792e5f7f71ce9320b341fe02135077e00fa3e21/g' feeds/packages/net/smartdns/Makefile
 # sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
 
-########### 更新lean的内置的open-vm-tools版本 ###########
-sed -i 's/11.3.0/12.2.5/g' feeds/packages/utils/open-vm-tools/Makefile
-sed -i 's/472d7c69cd4b344e9ced18962cebdd3943fd5f10/8e4cfd94553cfeeb45b195864a30ccb6b18d17ef/g' feeds/packages/utils/open-vm-tools/Makefile
-sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/utils/open-vm-tools/Makefile
-
 ########### 安装smartdns（必选）###########
 # git clone https://github.com/PikuZheng/smartdns.git package/smartdns
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
