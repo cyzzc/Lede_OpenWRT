bb54882f87bbbb9288fe1e66b01708094272e3d7
cyzzc <80954721+cyzzc@users.noreply.github.com>
2024-02-27 15:26:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,9 +20,9 @@ sed -i "s/hostname='.*'/hostname='SZZzc_1288'/g" package/base-files/files/bin/co
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 ########### 更新lean的内置的smartdns版本 ###########
-sed -i 's/1.2023.42/1.2023.43/g' feeds/packages/net/smartdns/Makefile
-sed -i 's/ed102cda03c56e9c63040d33d4a391b56491493e/9ee27e7ba2d9789b7e007410e76c06a957f85e98/g' feeds/packages/net/smartdns/Makefile
-sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
+# sed -i 's/1.2023.42/1.2023.43/g' feeds/packages/net/smartdns/Makefile
+# sed -i 's/ed102cda03c56e9c63040d33d4a391b56491493e/9ee27e7ba2d9789b7e007410e76c06a957f85e98/g' feeds/packages/net/smartdns/Makefile
+# sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
 
 ########### 安装smartdns（必选）###########
 # git clone https://github.com/PikuZheng/smartdns.git package/smartdns
